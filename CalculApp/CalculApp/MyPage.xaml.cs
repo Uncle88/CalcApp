879815334d0c355b03b4cc41cc32b9e2989f98d9
Andrey Proskura <andreyproskura88@gmail.com>
2016-03-30 22:50:56 +0300

    mad calculator

---
+++ CalculApp/CalculApp/MyPage.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace CalculApp
+{
+	public partial class MyPage : ContentPage
+	{
+		public MyPage ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
+
