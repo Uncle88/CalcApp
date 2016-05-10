7bae5cb83fdb636c9b41e86dcaaeb9b3a92cebf1
Andrey Proskura <andreyproskura88@gmail.com>
2016-05-10 21:55:27 +0300

    functional implementation

--- CalculApp/CalculApp/MyPage.xaml.cs
+++ CalculApp/CalculApp/MyPage.xaml.cs
@@ -7,9 +7,21 @@ namespace CalculApp
 {
 	public partial class MyPage : ContentPage
 	{
+		 const int PERSENTEG = 17;
+
 		public MyPage ()
 		{
 			InitializeComponent ();
+			_button.Clicked += _button_Clicked;
+		}
+
+		void _button_Clicked (object sender, EventArgs e)
+		{
+			var	text = (double.Parse (_entry.Text)).ToString();
+
+			var	_tip = (text * 17 / 100).ToString ();
+
+			var	_totalSum = (text + _tip).ToString();
 		}
 	}
 }
